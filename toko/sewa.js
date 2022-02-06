a69453576069a2fac158ea73d4bf6caf66a39cf1
AWIEE-STORE <96707390+AWIEE-STORE@users.noreply.github.com>
2022-02-06 07:53:08 +0700

    Update and rename shop/sewa.js to toko/sewa.js

---
+++ toko/sewa.js
@@ -0,0 +1,28 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 3.000
+⬡ 2 MINGGU = Rp 5.000
+⬡ 3 MINGGU = Rp 7.000
+⬡ 1 BULAN = Rp 8.000
+⬡ PERMANEN = Rp 10.000
+⬡ PERMANEN + USER PREM = Rp 15.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 20K
+
+
+⬣ MINAT? PM
+⬡ wa.me/62896545070096
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
