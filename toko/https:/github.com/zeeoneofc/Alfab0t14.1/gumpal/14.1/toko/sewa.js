9759e372ff7168212a054846d3c1af39b71e6acc
AWIEE-STORE <96707390+AWIEE-STORE@users.noreply.github.com>
2022-02-07 11:29:52 +0700

    Create sewa.js

---
+++ toko/https:/github.com/zeeoneofc/Alfab0t14.1/gumpal/14.1/toko/sewa.js
@@ -0,0 +1,28 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 2.000
+⬡ 2 MINGGU = Rp 3.000
+⬡ 3 MINGGU = Rp 4.000
+⬡ 1 BULAN = Rp 5.000
+⬡ PERMANEN = Rp 10.000
+⬡ PERMANEN + USER PREM = Rp 15.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/62896545070096
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
