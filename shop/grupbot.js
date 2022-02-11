182fcf665e513dbfe5c49015b2331b306d02b68e
AWIEE-BOTZZ <96273425+AWIEE-BOTZZ@users.noreply.github.com>
2022-02-11 08:43:21 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,13 +3,15 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
+_https://chat.whatsapp.com/C0TOJVyfS6S18KCNjijF7A_
 2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
+_https://chat.whatsapp.com/K3JkfQJkbjc2VRnbiasQYY_
 3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
+_https://chat.whatsapp.com/KNNVuzWKhH12cxI5nbtlw9_
 4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+_https://chat.whatsapp.com/FgCyXvBlTAq4NxJuiAxNV5_
+5. *Alphabot Support 5* 
+_https://chat.whatsapp.com/JTAaJbJhLDXHBOHJvGiv5y_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
